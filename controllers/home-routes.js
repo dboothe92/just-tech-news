ec28a582fac02054e774da4c4a9054ceb0084866
Dan B <dbraunby@hotmail.com>
2021-04-22 12:21:01 -0600

    refactored code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,7 +33,7 @@ router.get('/', (req, res) => {
 
         res.render('homepage', {
             posts,
-            loggedIn: req.sessions.loggedIn
+            loggedIn: req.session.loggedIn
         });
     })
     .catch(err => {
